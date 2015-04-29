ed7a89031c899da86895ccba8ebb7aa3ee9d83da
Rafael Correia Paz <rafael.cp@outlook.com>
2015-04-28 23:30:58 -0300

    first commit

---
+++ app/view/main/Main.js
@@ -0,0 +1,69 @@
+/**
+ * This class is the main view for the application. It is specified in app.js as the
+ * "autoCreateViewport" property. That setting automatically applies the "viewport"
+ * plugin to promote that instance of this class to the body element.
+ *
+ * TODO - Replace this content of this view to suite the needs of your application.
+ */
+Ext.define('Wap.view.main.Main', {
+    extend: 'Ext.container.Container',
+    requires: [
+        'Wap.view.main.MainController',
+        'Wap.view.main.MainModel'
+    ],
+
+    xtype: 'app-main',
+    
+    controller: 'main',
+    viewModel: {
+        type: 'main'
+    },
+
+    layout: {
+        type: 'border'
+    },
+
+    items: [{
+        xtype: 'panel',
+        bind: {
+            title: '{name}'
+        },
+        region: 'west',
+        width: 250,
+        split: true,
+        items:
+        [
+            {
+                xtype: 'treepanel',
+                root: {
+                    children: [{
+                        text: 'Europe, ME, Africa',
+                        children: [{
+                            text: 'UK of GB & NI',
+                            children: [{
+                                text: 'London',
+                                leaf: true
+                            }]
+                        }]
+                    }, {
+                        text: 'North America',
+                        children: [{
+                            text: 'USA',
+                            children: [{
+                                text: 'Redwood City',
+                                leaf: true
+                            }]
+                        }]
+                    }]
+                }
+            }
+        ]
+    },{
+        region: 'center',
+        xtype: 'tabpanel',
+        items:[{
+            title: 'Tab 1',
+            html: '<h2>Content appropriate for the current navigation.</h2>'
+        }]
+    }]
+});
---
+++ app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('Wap.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'Wap'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
